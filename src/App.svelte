9a181c00240d390a72dd2ba0b37990b955016767
Pierre Munck <pierre.munck@gmail.com>
2021-02-22 09:48:56 +0100

    pagina de saldo

---
+++ src/App.svelte
@@ -0,0 +1,57 @@
+<script>
+	import ListInvoice from './ListInvoice.svelte';
+	const amount = 25
+	let amountPrint = `${amount} $`;
+	const dolares = () => {
+		amountPrint = `${amount} $`;
+	} 
+	const cordoba = () => {
+		amountPrint = `${amount * 35} c$`;
+	}
+	const listInvoice = {
+		titleList : 'Invoice',
+		listItem : [
+			{ value : 'item 1'},
+			{ value : 'item 2'},
+			{ value : 'item 3'}
+		]
+	}
+	let toggle = false;
+	const togglelist = () => {
+		toggle = !toggle;
+	}
+</script>
+
+<main>
+	<h1>{amountPrint}</h1>
+	<button on:click={cordoba}>cordoba</button>
+	<button on:click={dolares}>dolares</button>
+	<button on:click={togglelist}>open invoice</button>
+	<ListInvoice {...listInvoice}, open = {toggle}/>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	h2 {
+		color: blue;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/ListInvoice.svelte
@@ -0,0 +1,27 @@
+<script>
+    export let titleList = 'titleList';
+    export let listItem = [];
+    export let open = false;
+</script>
+<section class={open ? "visible" : 'invisible'}>
+<h2>{titleList}</h2>
+    <ul>
+        {#each listItem as item}
+        <li>{item.value}</li>
+        {/each}
+    </ul>
+</section>
+
+<style>
+	h2 {
+        color: chartreuse;
+    }
+
+    .visible {
+       padding : 10px;
+    }
+
+    .invisible {
+        display: none;
+    }
+</style>
